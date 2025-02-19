66c8edf01914371be9854ce1ed5cfd8bf12da858
tordbm <tordbm@icloud.com>
2025-02-19 16:26:17 +0100

    use docker local database in dev mode

--- app/config.py
+++ app/config.py
@@ -5,10 +5,14 @@ from sqlalchemy import URL
 
 load_dotenv()
 
-DATABASE_URL = URL.create(
-    "postgresql+asyncpg",
-    username=os.getenv("DATABASE_USER"),
-    password=os.getenv("DATABASE_PASSWORD"),
-    host=os.getenv("DATABASE_HOST"),
-    database=os.getenv("DATABASE_NAME"),
+DATABASE_URL = (
+    URL.create(
+        "postgresql+asyncpg",
+        username=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),
+        host=os.getenv("DATABASE_HOST"),
+        database=os.getenv("DATABASE_NAME"),
+    )
+    if os.getenv("ENVIRONMENT") == "production"
+    else os.getenv("DATABASE_URL")
 )
