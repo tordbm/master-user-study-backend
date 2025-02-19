af09462d15c1c4ab527da28293ed1d2eea513478
tordbm <tordbm@icloud.com>
2025-02-19 18:30:55 +0100

    enable pool_pre_ping to prevent from sleeping

--- app/database.py
+++ app/database.py
@@ -3,7 +3,14 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+    pool_size=20,
+    max_overflow=10,
+)
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
