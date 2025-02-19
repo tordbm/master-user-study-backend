c836efb78b2e1558d67bdfa56b9f12a88d6a68ea
tordbm <tordbm@icloud.com>
2025-02-19 19:58:05 +0100

    Revert "enable pool_pre_ping to prevent from sleeping"
    This reverts commit af09462d15c1c4ab527da28293ed1d2eea513478.

--- app/database.py
+++ app/database.py
@@ -3,14 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import DATABASE_URL
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=True,
-    pool_pre_ping=True,
-    pool_recycle=1800,
-    pool_size=20,
-    max_overflow=10,
-)
+engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
